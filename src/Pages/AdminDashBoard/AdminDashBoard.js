0d370d21ba3eebdebf8ba789d98f7c3040c3822a
Base <orockbessonglawrence@gmail.com>
2023-03-07 19:56:49 +0000

    Added a data file for Dummy data used in Admin dashboard

--- src/Pages/AdminDashBoard/AdminDashBoard.js
+++ src/Pages/AdminDashBoard/AdminDashBoard.js
@@ -1,4 +1,8 @@
-import React from 'react';
+import React,{useEffect, useState} from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { FiSettings } from 'react-icons/fi';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+
 import Footer from '../../components/Footer';
 
 const AdminDashBoard = () => {
