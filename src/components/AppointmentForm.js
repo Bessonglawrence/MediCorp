840f4fedcc640f67070f6cee149822ded303a74a
Base <orockbessonglawrence@gmail.com>
2023-03-29 21:18:51 +0100

    fixing deployment errors

--- src/components/AppointmentForm.js
+++ src/components/AppointmentForm.js
@@ -4,7 +4,6 @@ import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Row from 'react-bootstrap/Row';
-import Select from 'react-select';
 import "../App.css";
 import { Referees, BloodTest, BoneTest, UltraSound } from '../Data/Data';
 
