0dde278d802767051b5564a7afa9951923e98125
Base <orockbessonglawrence@gmail.com>
2023-04-01 11:49:44 +0100

    Removed header menu button

--- src/Pages/LayOut/LayOut.js
+++ src/Pages/LayOut/LayOut.js
@@ -14,9 +14,6 @@ const LayOut = () => {
         <>
           <nav className="navbar navbar-expand-lg bg-light sticky-top" id="topnavbar">
             <a className="navbar-brand" href="#"><img src={logo} height="60" onClick={() => reloadPage()} /></a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
             {/* <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav ml-auto">
                 <li className="nav-item active">
