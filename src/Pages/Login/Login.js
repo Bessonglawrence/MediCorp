25f03661f98ddb41ef3001f0550d794964dfaaf7
Base <orockbessonglawrence@gmail.com>
2023-04-19 15:12:31 +0100

    Styled login page

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,4 +1,12 @@
-import React from 'react'
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Row from 'react-bootstrap/Row';
+import Spinner from 'react-bootstrap/Spinner';
+import Select from 'react-select';
+import logo from '../../Images/logo.png';
 
 const Login = () => {
   return (
@@ -13,29 +21,38 @@ const Login = () => {
 
                       <div className="text-center">
                         
-                        <h4 className="mt-1 mb-5 pb-1">We are The Lotus Team</h4>
+                        <h4 className="mt-1 mb-5 pb-1" style={{fontStyle: 'italic'}}>LOGIN AS ADMIN</h4>
                       </div>
 
-                      <form>
+                      <Form>
 
-                        <div className="form-outline mb-4">
-                        <label className="form-label" >Email</label>
-                          <input 
-                            type="email" id="form2Example11" 
-                            className="form-control"
-                            placeholder="Admin Email" 
+                        <Form.Group as={Col} className="mb-4" controlId="validationCustomPhoneNumber">
+                          <Form.Label>Email</Form.Label>
+                          <Form.Control
+                              required
+                              type="email"
+                              placeholder="E.g john@gmail.com"
+                              name="email"
                           />
-                        </div>
+                          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                        </Form.Group>
 
-                        <div className="form-outline mb-4">
-                          <label className="form-label" >Password</label>
-                          <input type="password" id="form2Example22" className="form-control" placeholder='******'/>
-                        </div>
+                        <Form.Group as={Col} className="mb-2" controlId="validationCustomPhoneNumber">
+                          <Form.Label>password</Form.Label>
+                          <Form.Control
+                              required
+                              type="password"
+                              placeholder="*******"
+                              name="password"
+                          />
+                          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                        </Form.Group>
+
 
                         <div className="text-center pt-1 mb-5 pb-1">
-                          <button className="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3" type="button">Log
-                            in</button>
-                          <a className="text-muted" href="#!">Forgot password?</a>
+                            <Button variant="primary" size="lg" className='gradient-custom-2 pl-6 pr-6' onClick={() => alert('Button click')}>
+                                Login
+                            </Button>
                         </div>
 
                         {/* <div className="d-flex align-items-center justify-content-center pb-4">
@@ -43,14 +60,16 @@ const Login = () => {
                           <button type="button" className="btn btn-outline-danger">Create new</button>
                         </div> */}
 
-                      </form>
+                      </Form>
 
                     </div>
                   </div>
                   <div className="col-lg-6 d-flex align-items-center gradient-custom-2">
                     <div className="text-white px-3 py-4 p-md-5 mx-md-4">
-                      <h4 className="mb-4">We are more than just a company</h4>
-                      <p className="small mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    <img src={logo}
+                    style={{width: 205, height: 120 }}alt="logo" />
+                      <h4 className="mb-4">Our Services are more than just Tests</h4>
+                      <p className="medium mb-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
                         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
                         exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                     </div>
